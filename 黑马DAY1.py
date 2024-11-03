7ebe2506b85647ddd29252be52ca617cd60e9b3b
Muyan-Chen <chenmy0814@gmail.com>
2024-11-03 23:25:43 +0100

    黑马DAY1

---
+++ 黑马DAY1.py
@@ -0,0 +1,9 @@
+print("Hallo world")
+# 字面量：在代码中能被写下来的值：number,string,list,tuple,set,dictionary
+# 注释的作用：更容易理解，增强代码的可读性
+# 单行注释：#开头，一般用一个空格隔开
+# 多行注释："""开头，"""结尾
+# 变量# ：
+money = 50
+print("I have "+str(money)+" euro")
+print("I have ", money ,"euro")
